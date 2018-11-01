a0467c35b6fa771f7b3743789e25d6110149bbc5
Manabu Niseki <manabu.niseki@gmail.com>
2018-11-01 09:59:28 +0900

    feat: add type validation

--- lib/securitytrails/clients/history.rb
+++ lib/securitytrails/clients/history.rb
@@ -4,6 +4,8 @@ module SecurityTrails
   module Clients
     class History < Client
       def get_dns_history(hostname, type, page = 1)
+        raise ArgumentError, "The API currently supports a, aaaa, mx, ns, soa and txt records." unless valid_type?(type)
+
         get("/history/#{hostname}/dns/#{type.downcase}", page: page) { |json| json }
       end
 
@@ -25,6 +27,12 @@ module SecurityTrails
 
       def get_whois_history(hostname)
         get("/history/#{hostname}/whois/") { |json| json }
+      end
+
+      private
+
+      def valid_type?(type)
+        ["a", "aaaa", "mx", "ns", "soa", "txt"].include? type.downcase
       end
     end
   end
